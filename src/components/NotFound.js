dc60be7a4ae8bda254530b7cffad8ffc4b9fea32
Nicolas Chenet <nicolas@oyst.com>
2017-02-11 21:25:19 +0100

    Components | NotFound

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import './NotFound.css';
+
+import React from 'react';
+
+export default function NotFound () {
+  return (
+    <div className="NotFound">
+      <h1 className="NotFound-Header">
+        <span className="NotFound-Header--code">404</span> 
+        Party not found
+      </h1>
+    </div>
+  )
+}
