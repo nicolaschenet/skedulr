642151c68632cd0200da5e929b7728fc0804821b
Nicolas Chenet <nicolas@oyst.com>
2017-02-12 16:55:29 +0100

    AppBar | Cosmetic tweaks

--- src/components/Site.js
+++ src/components/Site.js
@@ -7,6 +7,8 @@ import {connectProfile, logout} from '../auth';
 
 import ActionAccountCircle from 'material-ui/svg-icons/action/account-circle';
 import AppBar from 'material-ui/AppBar';
+import Avatar from 'material-ui/Avatar';
+import Divider from 'material-ui/Divider';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import {Link} from 'react-router';
@@ -24,6 +26,17 @@ const Logged = props => (
     targetOrigin={{horizontal: 'right', vertical: 'center'}}
     anchorOrigin={{horizontal: 'right', vertical: 'center'}}
   >
+    <MenuItem 
+      insetChildren
+      primaryText={`Logged as ${props.profile.name}`}
+    >
+      <Avatar size={40} src={props.profile.picture} style= {{
+        position: 'absolute',
+        left: '15px',
+        top: '4px'
+      }} />
+    </MenuItem>
+    <Divider />
     <MenuItem primaryText="View profile" onClick={() => browserHistory.push('/profile/edit')}/>
     <MenuItem primaryText="Sign out" onClick={() => logout()} />
   </IconMenu>
@@ -47,7 +60,7 @@ class Site extends Component {
     const style = {
       appBar: {
         root: {
-          position: 'absolute'
+          position: 'fixed'
         },
         title: {
           fontSize: '1rem',
