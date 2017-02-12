815c1266202e8d4822cc9753aa80319de4215393
Nicolas Chenet <nicolas@oyst.com>
2017-02-12 16:53:56 +0100

    Home | Cosmetic tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,25 @@
-/* eslint no-undef:off */
-import './Home.css';
+import './Home.css'
 
-import FlatButton from 'material-ui/FlatButton';
-import React from 'react';
+import FlatButton from 'material-ui/FlatButton'
+import React from 'react'
+import rocket from '../images/rocket.svg'
 
 export default function Home () {
   return (
     <div className='Home'>
-      <h2 className='Home-Header'>
-        <span className='Home-Header--title'>Skedulr</span>
-        is coming
-      </h2>
-      <FlatButton 
-        label="View an example party" 
-        secondary={true} 
-        href='parties/-KcVQxXvUq8Coac1iixz'
-      />
+      <div></div>
+      <div>
+        <h2 className='Home-Header'>
+          <span className='Home-Header--title'>Skedulr</span>
+          is coming
+        </h2>
+        <FlatButton 
+          label="View an example party" 
+          secondary={true} 
+          href='parties/-KcVQxXvUq8Coac1iixz'
+        />
+      </div>
+      <img src={rocket} />
     </div> 
   )
 }
