4ce44d7b833289c5558427ec866abb78af57a7a5
Nicolas Chenet <nicolas@oyst.com>
2017-02-12 16:55:00 +0100

    NotFound | Here comes the seagull

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,14 +1,16 @@
 import './NotFound.css';
 
 import React from 'react';
+import seagul from '../images/seagul.svg'
 
 export default function NotFound () {
   return (
     <div className="NotFound">
       <h1 className="NotFound-Header">
         <span className="NotFound-Header--code">404</span> 
-        Party not found
+        You're lost
       </h1>
+      <img src={seagul} />
     </div>
   )
 }
